314f791c765f5438db39507cb8370210ac340742
VasysMS <vasiavasia10000@gmail.com>
2024-02-17 18:31:49 +0300

    Update cam-viewer lib to 0.0.2 and added debugging functions

--- streetcat_console.py
+++ streetcat_console.py
@@ -3,22 +3,45 @@ import json
 
 with open('streetcat_viewer/cams.json', 'r') as cams:
     cams_json = json.loads(cams.read())
+command = "connect"
+debug = True
+
+if debug:
+    print("Commands:\nconnect\ndata\nurl\ntime")
+    command = input("\nEnter command: ")
+
+def cams_list():
+    print("\nCats Cams:")
+    for key in cams_json.keys(): print(key)
+    cam_name = input("\nEnter camera name: ")
+    cam_number = input("Cam number: ")
+    return [cam_name, cam_number]
 
 while(True):
-    print("Cats Cams:")
-    for key in cams_json.keys(): print(key)
-    
-    print("Enter camera name: ", end="")
-    cam_name = input()
-    print("Cam number: ", end="")
-    cam_number = input()
-
-    player = cam_viewer.playback(command = "ffplay", 
-                        parameters = "", 
-                        cams_json = cams_json,
-                        cam_name = cam_name, 
-                        cam_number = int(cam_number),
-                        use_text = True,
-                        fontfile = "./CALIBRI.TTF")
-    player[0].wait()
-    print(player[1])
+    responce = ""
+    if command == "connect":
+        cam = cams_list()
+        player = cam_viewer.playback(command = "ffplay", 
+                            parameters = "", 
+                            cams_json = cams_json,
+                            cam_name = cam[0], 
+                            cam_number = int(cam[1]),
+                            use_text = True,
+                            font_file = "./CALIBRI.TTF")
+        responce = player[1]
+        print(responce)
+        player[0].wait()
+        print(f"\nCam {cam[0]} {cam[1]} is closed")
+    elif command == "data":
+        cam = cams_list()
+        data = cam_viewer.cam_data(cams_json, cam[0], int(cam[1]))
+        print(f"\nURL: {data[0]}\nEnabled: {data[1]}\nResponce: {data[2]}\n")
+    elif command == "url":
+        url = input("\nEnter URL: ")
+        print(f"Available: {cam_viewer.url_available(url)}\n")
+    elif command == "time":
+        input("\nPress enter to display time...")
+        print(f"Current time: {cam_viewer.current_time()}")
+    else: 
+        print("Command does not exist!!!\nSetting the command to connect...")
+        command = "connect"
